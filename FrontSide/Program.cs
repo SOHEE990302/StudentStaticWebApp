bbfcf00e69bb19a8ab5240e242eeb4df7c5fb97a
hwangs <shwang67@my.bcit.ca>
2025-02-18 09:53:38 -0800

    ss

--- FrontSide/Program.cs
+++ FrontSide/Program.cs
@@ -8,22 +8,37 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Azure 및 로컬 환경 감지
 string apiBaseUrl;
 
-if (builder.HostEnvironment.IsDevelopment()) // 로컬 실행 시
+if (builder.HostEnvironment.IsDevelopment())
 {
     apiBaseUrl = "http://localhost:7071/api/";
 }
-else // 배포된 환경에서는 Azure Static Web Apps API URL 사용
+else
 {
-    apiBaseUrl = "https://purple-rock-0829d3d10.4.azurestaticapps.net/api/"; // 너의 Azure Static Web Apps 주소
+    apiBaseUrl = "https://purple-rock-0829d3d10.4.azurestaticapps.net/api/";
 }
 
-// HttpClient 설정
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 
-// StudentService 등록
 builder.Services.AddScoped<StudentService>();
 
 await builder.Build().RunAsync();
+
+// using Microsoft.AspNetCore.Components.Web;
+// using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+// using FrontSide;
+// using System.Net.Http;
+// using System;
+
+// var builder = WebAssemblyHostBuilder.CreateDefault(args);
+// builder.RootComponents.Add<App>("#app");
+// builder.RootComponents.Add<HeadOutlet>("head::after");
+
+// // Ensure the correct API URL
+// builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:7071/api/") });
+
+// // Register StudentService
+// builder.Services.AddScoped<StudentService>();
+
+// await builder.Build().RunAsync();
