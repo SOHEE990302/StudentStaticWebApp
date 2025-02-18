dc8844163da2e8820877c6bc7adf6269118ae59a
hwangs <shwang67@my.bcit.ca>
2025-02-18 09:05:09 -0800

    edit domain

--- FrontSide/Program.cs
+++ FrontSide/Program.cs
@@ -8,19 +8,22 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// Azure 및 로컬 환경 감지
 string apiBaseUrl;
 
-if (builder.HostEnvironment.IsDevelopment())
+if (builder.HostEnvironment.IsDevelopment()) // 로컬 실행 시
 {
     apiBaseUrl = "http://localhost:7071/api/";
 }
-else
+else // 배포된 환경에서는 Azure Static Web Apps API URL 사용
 {
-    apiBaseUrl = "https://staticwebappsh.azurestaticapps.net/api/";
+    apiBaseUrl = "https://purple-rock-0829d3d10.4.azurestaticapps.net/api/"; // 커스텀 도메인 사용
 }
 
+// HttpClient 설정
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 
+// StudentService 등록
 builder.Services.AddScoped<StudentService>();
 
 await builder.Build().RunAsync();
