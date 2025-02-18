4a53452980df4cac337913e0749e61dc0481ddde
hwangs <shwang67@my.bcit.ca>
2025-02-18 09:02:35 -0800

    dd

--- FrontSide/Program.cs
+++ FrontSide/Program.cs
@@ -8,22 +8,19 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-// Azure 및 로컬 환경 감지
 string apiBaseUrl;
 
-if (builder.HostEnvironment.IsDevelopment()) // 로컬 실행 시
+if (builder.HostEnvironment.IsDevelopment())
 {
     apiBaseUrl = "http://localhost:7071/api/";
 }
-else // 배포된 환경에서는 Azure Static Web Apps API URL 사용
+else
 {
-    apiBaseUrl = "https://staticwebappsh.azurestaticapps.net/api/"; // 너의 Azure Static Web Apps 주소
+    apiBaseUrl = "https://staticwebappsh.azurestaticapps.net/api/";
 }
 
-// HttpClient 설정
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 
-// StudentService 등록
 builder.Services.AddScoped<StudentService>();
 
 await builder.Build().RunAsync();
